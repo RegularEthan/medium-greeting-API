88f61fb5ce80b9d63234daab928eb7ed5753b67c
RegularEthan <134470294+RegularEthan@users.noreply.github.com>
2023-12-24 14:59:58 +1100

    Update Howdy.py
    Changed the code to register a product instead of a name

--- Howdy.py
+++ Howdy.py
@@ -2,24 +2,24 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/getmsg', methods=['GET'])
+@app.route('/getproduct', methods=['GET'])
 def respond():
     # Retrieve the name from the url parameter ?name=
-    name = request.args.get("name", None)
+    name = request.args.get("product", None)
 
     # For debugging
-    print(f"Received: {name}")
+    print(f"Received: {product}")
 
     response = {}
 
     # Check if the user sent a name at all
-    if not name:
+    if not product:
         response["ERROR"] = "No name found. Please send a name."
     # Check if the user entered a number
     elif str(name).isdigit():
         response["ERROR"] = "The name can't be numeric. Please send a string."
     else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
+        response["MESSAGE"] = f"Welcome {product} to our awesome API!"
 
     # Return the response in json format
     return jsonify(response)
