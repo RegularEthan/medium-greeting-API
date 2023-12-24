31a54166f59c75263e7376e3a093b4633c51c33b
RegularEthan <134470294+RegularEthan@users.noreply.github.com>
2023-12-24 15:23:25 +1100

    Update Howdy.py
    Using chat GPT code

--- Howdy.py
+++ Howdy.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-
 @app.route('/getproduct', methods=['GET'])
 def respond():
     # Retrieve the name from the url parameter ?name=
@@ -16,14 +15,13 @@ def respond():
     if not product:
         response["ERROR"] = "No name found. Please send a name."
     # Check if the user entered a number
-    elif str(name).isdigit():
+    elif str(product).isdigit():
         response["ERROR"] = "The name can't be numeric. Please send a string."
     else:
         response["MESSAGE"] = f"Welcome {product} to our awesome API!"
 
     # Return the response in json format
     return jsonify(response)
-
 
 @app.route('/post/', methods=['POST'])
 def post_something():
@@ -32,7 +30,7 @@ def post_something():
     # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
     if param:
         return jsonify({
-            "Message": f"Welcome {product} to our awesome APIS!",
+            "Message": f"Welcome {param} to our awesome APIS!",
             # Add this option to distinct the POST request
             "METHOD": "POST"
         })
@@ -41,12 +39,10 @@ def post_something():
             "ERROR": "No name found. Please send a name."
         })
 
-
 @app.route('/')
 def index():
     # A welcome message to test our server
     return "<h1>Welcome to our medium-greeting-api!</h1>"
-
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
